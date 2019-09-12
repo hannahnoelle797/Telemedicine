e8b5d46798bb55fca82127d1d090569a2f13a728
samcwood <samcwood57@gmail.com>
2019-09-12 16:39:16 -0400

    Changes to Appointments page. To be updated further.

--- app/src/main/java/com/example/telemedicine/ui/appointments/AppointmentsFragment.java
+++ app/src/main/java/com/example/telemedicine/ui/appointments/AppointmentsFragment.java
@@ -1,4 +1,35 @@
 package com.example.telemedicine.ui.appointments;
 
-public class AppointmentsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.telemedicine.R;
+import com.example.telemedicine.ui.appointments.AppointmentsViewModel;
+
+public class AppointmentsFragment extends Fragment {
+    private AppointmentsViewModel appointmentsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        appointmentsViewModel =
+                ViewModelProviders.of(this).get(AppointmentsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_appointments, container, false);
+        final TextView textView = root.findViewById(R.id.text_upcoming_appointments);
+        appointmentsViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
 }
--- app/src/main/java/com/example/telemedicine/ui/appointments/AppointmentsViewModel.java
+++ app/src/main/java/com/example/telemedicine/ui/appointments/AppointmentsViewModel.java
@@ -1,4 +1,17 @@
 package com.example.telemedicine.ui.appointments;
 
-public class AppointmentsViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AppointmentsViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public AppointmentsViewModel() {
+        mText = new MutableLiveData<>();
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
