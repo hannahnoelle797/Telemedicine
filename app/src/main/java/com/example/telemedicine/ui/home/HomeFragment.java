0c163350cc85e058045a27a5d0533f57ef4378a5
hannahnoelle797 <hannahnoelle797@gmail.com>
2019-09-19 15:55:16 -0400

    Created listener for recycler view items. Still need to implement methods

--- app/src/main/java/com/example/telemedicine/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/telemedicine/ui/home/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.telemedicine.ui.home;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,22 +16,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.telemedicine.R;
 import com.example.telemedicine.ui.utilities.RecyclerItem;
+import com.example.telemedicine.ui.utilities.RecyclerItemClickListener;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private String[] myDataset = {"Item 1", "Item 2", "Item 3"};
-    //Activity activity = (Activity)getContext();
+    private RecyclerView recyclerView_appt, recyclerView_chat, recyclerView_report;
+    private RecyclerView.Adapter mAdapter_appt, mAdapter_chat, mAdapter_report;
+    private RecyclerView.LayoutManager layoutManager_appt, layoutManager_chat, layoutManager_report;
+    private String[] apptData = {"Physical - 9/29 @ 10:00am", "Vaccination - 10/4 @ 1:30pm", "Check-Up - 10/19 @ 9:00am"};
+    private String[] chatData = {"Dr. Jane Smith", "Dr. Hayden Lee", "Dr. Michael Dean"};
+    private String[] reportData = {"Blood Work 9/10", "Vaccination Summary 9/1", "Physical 8/23"};
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.editText);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -40,12 +42,48 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        recyclerView = (RecyclerView)root.findViewById(R.id.recycler);
-        layoutManager = new LinearLayoutManager();
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mAdapter = new RecyclerItem(myDataset);
-        recyclerView.setAdapter(mAdapter);
+        recyclerView_appt = (RecyclerView)root.findViewById(R.id.recycler_appts);
+        layoutManager_appt = new LinearLayoutManager(this.getActivity());
+        recyclerView_appt.setLayoutManager(layoutManager_appt);
+        mAdapter_appt = new RecyclerItem(apptData);
+        recyclerView_appt.setAdapter(mAdapter_appt);
+
+        recyclerView_appt.addOnItemTouchListener(
+                new RecyclerItemClickListener(getContext(), recyclerView_appt ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        // do whatever
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+                        // do whatever
+                    }
+                })
+        );
+
+        recyclerView_chat = (RecyclerView)root.findViewById(R.id.recycler_chats);
+        layoutManager_chat = new LinearLayoutManager(this.getActivity());
+        recyclerView_chat.setLayoutManager(layoutManager_chat);
+        mAdapter_chat = new RecyclerItem(chatData);
+        recyclerView_chat.setAdapter(mAdapter_chat);
+
+        recyclerView_report = (RecyclerView)root.findViewById(R.id.recycler_reports);
+        layoutManager_report = new LinearLayoutManager(this.getActivity());
+        recyclerView_report.setLayoutManager(layoutManager_report);
+        mAdapter_report = new RecyclerItem(reportData);
+        recyclerView_report.setAdapter(mAdapter_report);
 
         return root;
     }
+/*
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.recycler_item:
+                Intent intent = new Intent(HomeFragment.this, AppointmentsFragment.class);
+                startActivity(intent);
+                break;
+            default:
+                System.out.println("Default");
+        }
+    }*/
 }
