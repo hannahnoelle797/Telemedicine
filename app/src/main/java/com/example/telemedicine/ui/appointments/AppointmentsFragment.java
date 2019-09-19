0773f31d0b9b3b4cbe41b0f080b9b9bf7c8fda38
samcwood <samcwood57@gmail.com>
2019-09-19 15:31:48 -0400

    Appoinments fragment Front-End changes Near finish. RecyclerViews added.

--- app/src/main/java/com/example/telemedicine/ui/appointments/AppointmentsFragment.java
+++ app/src/main/java/com/example/telemedicine/ui/appointments/AppointmentsFragment.java
@@ -1,5 +1,6 @@
 package com.example.telemedicine.ui.appointments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.telemedicine.R;
-import com.example.telemedicine.ui.appointments.AppointmentsViewModel;
+import com.example.telemedicine.ui.utilities.RecyclerItem;
 
 public class AppointmentsFragment extends Fragment {
     private AppointmentsViewModel appointmentsViewModel;
+    private RecyclerView recyclerView;
+    private RecyclerView recyclerView2;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.LayoutManager layoutManager2;
+    private String[] myDataset = {"Item 1", "Item 2", "Item 3"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +39,16 @@ public class AppointmentsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        recyclerView = (RecyclerView)root.findViewById(R.id.recycler);
+        recyclerView2 = (RecyclerView)root.findViewById(R.id.recycler2);
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        layoutManager2 = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView2.setLayoutManager(layoutManager2);
+        mAdapter = new RecyclerItem(myDataset);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView2.setAdapter(mAdapter);
         return root;
     }
 }
