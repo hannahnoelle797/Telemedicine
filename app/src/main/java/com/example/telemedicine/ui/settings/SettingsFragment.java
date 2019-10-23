129371a0b0bcb615e27327608123b8609c0392f1
samcwood <samcwood57@gmail.com>
2019-10-23 16:04:49 -0400

    Setting activity and signout menu added.

--- app/src/main/java/com/example/telemedicine/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/telemedicine/ui/settings/SettingsFragment.java
@@ -3,23 +3,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import com.example.telemedicine.R;
 
+
 public class SettingsFragment extends Fragment {
-    private SettingsViewModel settingsViewModel;
+
+    private LinearLayout account_btn;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        account_btn = (LinearLayout)root.findViewById(R.id.account_btn);
+        account_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.print("KFEOJAOJ#R#OJT$OJT%O$J");
+            }
+        });
         return root;
     }
 }
