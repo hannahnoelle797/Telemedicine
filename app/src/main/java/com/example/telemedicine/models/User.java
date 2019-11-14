45c44914f0d766424bff435322a297d33c49e2f6
Drhatl95 <david.howard1100>
2019-11-13 20:06:21 -0500

    Added changes for signup

--- app/src/main/java/com/example/telemedicine/models/User.java
+++ app/src/main/java/com/example/telemedicine/models/User.java
@@ -3,58 +3,84 @@ package com.example.telemedicine.models;
 public class User {
 
     // Global Variables
-    String firstName, lastName, email, password, userID;
+    String fullName, email, password, userID, address, phoneNum, gender;
     int lastFourSSN;
 
     // Constructor
-    public User(String firebaseUserID, String firstName, String lastName, String email, String password, int lastFourSSN) {
+    public User(String firebaseUserID, String fullName, String email, String streetAddress, String phoneNum, String gender, int lastFourSSN) {
         this.userID = firebaseUserID;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.fullName = fullName;
         this.email = email;
-        this.password = password;
+        this.address = streetAddress;
+        this.phoneNum = phoneNum;
+        this.gender = gender;
         this.lastFourSSN = lastFourSSN;
     }
 
-    public User(String firebaseUserID, String fullName, String email) {
+    // Default Constructor
+    public User() {}
 
+    public String getFullName() {
+        return fullName;
     }
 
-    public User() {
-
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
-    // Accessor Methods
-    public String getFirstName() {
-        return this.firstName;
-    }
-    public String getLastName() {
-        return this.lastName;
-    }
     public String getEmail() {
-        return this.email;
+        return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
-        return this.password;
+        return password;
     }
-    public int getLastFourSSN() {
-        return this.lastFourSSN;
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getUserID() {
         return userID;
     }
 
-    // Mutator Methods
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
-    public void setEmail(String email) {
-        this.email = email;
+
+    public String getAddress() {
+        return address;
     }
-    public void setPassword(String password) {
-        this.password = password;
+
+    public void setAddress(String address) {
+        this.address = address;
     }
-    public void setLastFourSSN(int lastFourSSN) { this.lastFourSSN = lastFourSSN; }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getLastFourSSN() {
+        return lastFourSSN;
+    }
+
+    public void setLastFourSSN(int lastFourSSN) {
+        this.lastFourSSN = lastFourSSN;
+    }
 }
